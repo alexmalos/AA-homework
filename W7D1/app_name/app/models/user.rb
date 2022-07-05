eac2df52c97744bf94d404435c1f001f1f14b9f2
Alex Malos <malosalexander@gmail.com>
2022-07-05 10:01:00 -0400

    finished rails auth

--- W7D1/app_name/app/models/user.rb
+++ W7D1/app_name/app/models/user.rb
@@ -18,17 +18,17 @@ class User < ApplicationRecord
     end
 
     def reset_session_token!
-        session_token = User.generate_session_token
-        save!
-        session_token
+        self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
     end
 
     def ensure_session_token
-        session_token ||= User.generate_session_token
+        self.session_token ||= User.generate_session_token
     end
 
     def password=(password)
         @password = password
-        password_digest = BCrypt::Password.create(password)
+        self.password_digest = BCrypt::Password.create(password)
     end
 end
